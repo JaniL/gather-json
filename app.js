d16d70112c1736e5a1ea160036bede9359d9688b
Jani Luukko <jj.luukko@gmail.com>
2014-05-29 22:16:29 +0300

    The API now returns epoch time of last played game.
    Also available: some nice documentation

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
+// gather-json
+// -----------
+
+// IRC bot that brings the status of #gather.fi to the web!
+// Made by Jarppa.
+
+
+// The bot requires restify and irc modules from npm to work
 var restify = require('restify');
 var irc = require('irc');
 
 var botNick = 'botenjson';
 var channel = '#gather.fi';
 
+// Let's initiate the IRC client
 var client = new irc.Client('irc.quakenet.org', botNick, {
     channels: [channel],
     floodProtection: true,
 });
 
+// These are the defaults values that API returns if there's no info about #gather.fi's topic.
 var gatherTopic = {
-	// maxPlayers: 12,
 	map: "cp_gravelpit",
 	players: [],
-	// motd: "http://tf2.fi/gatherfi"
 };
 
+// This holds the time of last played gather. It changes when GatherFI announces that the game starts.
+var lastPlayed = 1;
 
-// Next cp_badlands: Snappe,V1llukka,josee,irppa,wrdo,Tunttu,Santtu 7/12 http://tf2.fi/gatherfi
+// This takes care of parsing the topic.
 var formatTopic = function(topic) {
 	var splitted = topic.split(' ');
 	var map = splitted[1].replace(':','');
 	var players = splitted[2].split(',');
+	// This happens if the topic has no players. Kinda clumsy.
 	if (players[0] == "0/12") {
 		players = [];
 	}
@@ -31,18 +42,21 @@ var formatTopic = function(topic) {
 	};
 };
 
-client.addListener('join' + channel, function(nick,message) {
-	if (nick == botNick) {
-		client.say(channel,'!hl off');
-	}
-});
-
+// Here's where the magic happens. When the topic changes, this triggers formatTopic() to parse it and saves it to gatherTopic variable.
 client.addListener('topic', function(channel,topic,nick,message) {
 	console.log(topic);
 	gatherTopic = formatTopic(topic);
 	console.log(gatherTopic);
 });
 
+client.addListener('message' + channel, function(nick,text,message) {
+	// If the GatherFI announces that the game starts, this will update the information about the last played game.
+	if (nick == "GatherFI" && text.indexOf("Gatheri alkaa!")) {
+		lastPlayed = new Date() / 1000;
+	}
+});
+
+// And here comes the restify stuff that hosts the API!
 function respond(req, res, next) {
   res.send(gatherTopic);
   next();
@@ -55,6 +69,7 @@ server.use(restify.gzipResponse());
 server.get('/gatherStatus', respond);
 server.head('/gatherStatus', respond);
 
+// BAM!
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
